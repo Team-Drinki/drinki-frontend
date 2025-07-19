687dd29ab299cf4da7157367af1bbc211389cebd
jongse7 <mmssjj72@naver.com>
2025-07-20 03:45:03 +0900

    feat: 로그인 페이지 뷰 완성 #9

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,32 +1,33 @@
 'use client';
 
-import CustomButton from '@/components/common/custom-button';
-import { SiNaver, SiGoogle } from 'react-icons/si';
-import { RiKakaoTalkFill } from 'react-icons/ri';
+import { SiNaver } from 'react-icons/si';
+import LoginButton from '@/components/buttons/login-button';
+import GoogleLogo from '@/components/svg/google-logo';
+import KakaoLogo from '@/components/svg/kakao-logo';
 
 export default function LoginPage() {
   return (
-    <main className="flex-1 flex flex-col items-center justify-center bg-white px-4">
-      <h1 className="text-head2 text-brown mb-8">Log in</h1>
-      <div className="w-full max-w-xs flex flex-col gap-4">
-        <CustomButton
-          icon={<RiKakaoTalkFill className="text-xl" />}
-          className="bg-[#FEE500] text-black hover:bg-[#e5ce00] font-semibold w-full"
+    <main className="flex-1 flex flex-col items-center justify-center bg-white mb-20">
+      <h1 className="text-head2 text-dark-brown mb-12">Log in</h1>
+      <div className="w-full max-w-xs flex flex-col gap-5">
+        <LoginButton
+          className="bg-[#FEE500] text-black hover:bg-[#e5ce00] font-semibold"
+          icon={<KakaoLogo className="size-[1.1rem]" />}
         >
-          카카오로 로그인
-        </CustomButton>
-
-        <CustomButton className="bg-[#03C75A] text-white hover:bg-[#02b24f] font-semibold w-full">
-          <SiNaver className="text-xl" />
-          네이버 ID로 로그인
-        </CustomButton>
-
-        <CustomButton
-          icon={<SiGoogle className="text-xl" />}
-          className="bg-[#F2F2F2] text-black hover:bg-[#e0e0e0] font-semibold w-full"
+          <p className="font-semibold text-sm">카카오로 로그인</p>
+        </LoginButton>
+        <LoginButton
+          className="bg-[#03C75A] text-white hover:bg-[#02b24f] font-semibold"
+          icon={<SiNaver className="size-3.5 ml-0.5" />}
         >
-          Google 계정으로 로그인
-        </CustomButton>
+          <p className="font-semibold text-sm">네이버 ID로 로그인</p>
+        </LoginButton>
+        <LoginButton
+          className="bg-[#F2F2F2] hover:bg-[#e0e0e0]"
+          icon={<GoogleLogo className="size-4.5" />}
+        >
+          <p className="font-semibold text-sm text-black">Google 계정으로 로그인</p>
+        </LoginButton>
       </div>
     </main>
   );
