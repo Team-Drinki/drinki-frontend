f628af37e0b4fd95d90686c02a8b10eace21b755
jongse7 <mmssjj72@naver.com>
2025-07-25 15:18:26 +0900

    fix: 시멘트 rule  라  header  nav  ㄱ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import './globals.css';
 import { ReactNode } from 'react';
 import { Inter } from 'next/font/google';
 import ReactQueryProvider from '@/lib/react-query-provider';
-import { Header } from '@/components/header/header';
 import { cn } from '@/lib/utils';
 import Footer from '@/components/Footer';
+import { Navibar } from '@/components/navibar/navibar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body className={cn(inter.className, 'flex flex-col min-h-screen')}>
         <ReactQueryProvider>
-          <Header />
+          <Navibar />
           {children}
           <Footer />
         </ReactQueryProvider>
