d59575b531bb709def37dd4413e1e9f20c823da6
jongse7 <mmssjj72@naver.com>
2025-08-11 01:43:36 +0900

    feat: 서치바 컴포넌트 제작

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+import BoardHeader from '@/components/common/BoardHeader';
+import Searchbar from '@/components/common/Searchbar';
+
 export default function CommunityPage() {
-  return <div>CommunityPage</div>;
+  return (
+    <div>
+      <BoardHeader />
+      <main className="pt-25 flex flex-col items-center">
+        <Searchbar />
+        <div className="h-16" />
+      </main>
+    </div>
+  );
 }
---
+++ src/components/common/BoardHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BoardHeader() {
+  return (
+    <header className="pb-[22px] pt-[85px] pl-40 bg-yellow-100">
+      <h1 className="text-head2 text-black">community</h1>
+      <h2 className="text-body1 text-black">사람들과 자유롭게 의견을 나눠보세요.</h2>
+    </header>
+  );
+}
