7609484b2ef41478a9933e8e1638ebbe44461a82
yongmin01 <heart77123@naver.com>
2025-11-20 17:41:00 +0900

    feat: 술 상세조회 api 연결

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    /* 술 상세페이지에 쓰이는 더미 이미지 (추후 제거 예정) */
+    domains: ['fastly.picsum.photos'],
+  },
 };
 
 export default nextConfig;
